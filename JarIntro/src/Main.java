42a670c1323dd1beee8b21db20077769d7813663
alialpay <63648362+alialpay@users.noreply.github.com>
2021-02-01 21:12:32 +0300

    Add files via upload

---
+++ JarIntro/src/Main.java
@@ -0,0 +1,11 @@
+import com.alialpay.matematik.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Marematik matematik = new Marematik();
+        matematik.toplama(3, 4);
+        matematik.çıkarma(33, 4);
+        matematik.çarpma(30, 4);
+        matematik.bölme(101, 4);
+    }
+}
