bf3924670f81ab9007b0bc759f4b027daf9df9c4
alialpay <alialpay96@hotmail.com>
2021-04-30 02:50:23 +0300

    references

---
+++ References/src/Test.java
@@ -0,0 +1,23 @@
+
+public class Test {
+    public static void main(String[] args) {
+  /*
+        Account account1 = new Account ("Ali","alialpay96@hotmail.com", "4896815");
+        
+        Account account2 = account1;
+        Account account3 = new Account();
+        
+        if (account1 == account3) {
+            System.out.println("Aynı objeyi gösteriyorlar");
+        }
+        else{
+            System.out.println("Aynı objeyi göstermiyorlar");
+        }
+      
+    } */
+  
+  new Account("ali","@hotmil.com", "865+5864").bilgilerigoster(); 
+  
+  
+    }
+}
