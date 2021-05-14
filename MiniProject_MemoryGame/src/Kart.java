c0c4093348d78c25778c3dcc4483b0eeb5d419ac
alialpay <alialpay96@hotmail.com>
2021-05-14 20:53:12 +0300

    mini project

---
+++ MiniProject_MemoryGame/src/Kart.java
@@ -0,0 +1,26 @@
+
+public class Kart {
+    private char deger;
+    private boolean tahmin = false;
+
+    public Kart(char deger) {
+        this.deger = deger;
+    }
+
+    public char getDeger() {
+        return deger;
+    }
+
+    public void setDeger(char deger) {
+        this.deger = deger;
+    }
+
+    public boolean isTahmin() {
+        return tahmin;
+    }
+
+    public void setTahmin(boolean tahmin) {
+        this.tahmin = tahmin;
+    }
+    
+}
---
+++ MiniProject_MemoryGame/src/Main.java
@@ -0,0 +1,166 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+ /*   private static Kart[][] kartlar = new Kart [4][4];
+
+    public static void main(String[] args) {
+                
+        kartlar[0][0] = new Kart( 'E' );
+        kartlar[0][1] = new Kart( 'A' );
+        kartlar[0][2] = new Kart( 'B' );
+        kartlar[0][3] = new Kart( 'F' );
+        kartlar[1][0] = new Kart( 'G' );
+        kartlar[1][1] = new Kart( 'A' );
+        kartlar[1][2] = new Kart( 'D' );
+        kartlar[1][3] = new Kart( 'B' );
+        kartlar[2][0] = new Kart( 'U' );
+        kartlar[2][1] = new Kart( 'R' );
+        kartlar[2][2] = new Kart( 'C' );
+        kartlar[2][3] = new Kart( 'U' );
+        kartlar[3][0] = new Kart( 'A' );
+        kartlar[3][1] = new Kart( 'L' );
+        kartlar[3][2] = new Kart( 'İ' );
+        kartlar[3][3] = new Kart( 'H' );
+        
+        //oyuntahtasi();
+        while (oyunbittimi() == false) {            
+            oyuntahtasi();
+            tahminet();
+        }
+        }
+        public static void tahminet(){
+            Scanner scanner = new Scanner(System.in);
+            
+            System.out.print("Birinci Tahmin (i ve j değerlerini bir boşluklu girin...)");
+            int i1 = scanner.nextInt();
+            int j1 = scanner.nextInt();
+            
+            kartlar[i1][j1].setTahmin(true);
+            oyuntahtasi();
+             System.out.print("İkinci Tahmin (i ve j değerlerini bir boşluklu girin...)");
+            int i2 = scanner.nextInt();
+            int j2 = scanner.nextInt();
+            
+            if (kartlar[i1][j1].getDeger() == kartlar[i2][j2].getDeger()) {
+                System.out.println("Doğru tahmin.Tebrikler!");
+                kartlar[i2][j2].setTahmin(true);
+                
+            }
+            else{
+                System.out.println("Yanlış tahmin...");
+               kartlar[i1][j1].setTahmin(false);
+            }
+        }
+        public static boolean oyunbittimi(){
+            for(int i =0; i < 4; i++){
+                for(int j =0; j<4; j++){
+                    if (kartlar[i][j].isTahmin() == false) {
+                        return false;
+                    }
+                }
+            }
+        return true;
+        }
+        
+        public static void oyuntahtasi() {
+            
+            for (int i = 0; i < 4; i++){
+                System.out.println("____________________");
+                for(int j = 0; j < 4; j++) {
+                    
+                    if (kartlar[i][j].isTahmin()) {
+                        System.out.print(" |" + kartlar[i][j].getDeger() + "| ");
+                    }
+                    else{
+                        System.out.print(" | | ");
+                    }
+                }
+                System.out.println("");
+            }
+            System.out.println("____________________");
+        
+ 
+    }*/
+    
+    
+    
+    private static final Kart[][] kartlar = new Kart[4][4];
+    
+    public static void main(String[] args) {
+        
+        
+        kartlar[0][0] = new Kart( 'E' );
+        kartlar[0][1] = new Kart( 'A' );
+        kartlar[0][2] = new Kart( 'B' );
+        kartlar[0][3] = new Kart( 'F' );
+        kartlar[1][0] = new Kart( 'G' );
+        kartlar[1][1] = new Kart( 'A' );
+        kartlar[1][2] = new Kart( 'D' );
+        kartlar[1][3] = new Kart( 'B' );
+        kartlar[2][0] = new Kart( 'U' );
+        kartlar[2][1] = new Kart( 'R' );
+        kartlar[2][2] = new Kart( 'C' );
+        kartlar[2][3] = new Kart( 'U' );
+        kartlar[3][0] = new Kart( 'A' );
+        kartlar[3][1] = new Kart( 'L' );
+        kartlar[3][2] = new Kart( 'İ' );
+        kartlar[3][3] = new Kart( 'H' );
+      
+        while (oyunbittimi() == false) {            
+            oyuntahtası();
+            tahminet();
+        }
+    }    
+        public static void tahminet(){
+            Scanner scanner = new Scanner(System.in);
+            
+            System.out.print("Birinci Tahmin (i ve j değerlerini bir boşluklu girin...)");
+            int i1 = scanner.nextInt();
+            int j1 = scanner.nextInt();
+            
+            kartlar[i1][j1].setTahmin(true);
+            oyuntahtası();
+             System.out.print("İkinci Tahmin (i ve j değerlerini bir boşluklu girin...)");
+            int i2 = scanner.nextInt();
+            int j2 = scanner.nextInt();
+            
+            if (kartlar[i1][j1].getDeger() == kartlar[i2][j2].getDeger()) {
+                System.out.println("Doğru tahmin.Tebrikler!");
+                kartlar[i2][j2].setTahmin(true);
+                
+            }
+            else{
+                System.out.println("Yanlış tahmin...");
+               kartlar[i1][j1].setTahmin(false);
+            }
+        
+    }
+    public static void oyuntahtası(){
+        for(int i = 0; i<4; i++){
+          System.out.println("- - - - - - - -");
+            for(int j = 0; j<4; j++){
+                if (kartlar[i][j].isTahmin()){
+                    System.out.print("|" + kartlar[i][j].getDeger() + "|");
+                }
+                else{
+                    System.out.print("| | ");
+                }
+            }System.out.println("");
+        }System.out.println("- - - - - - - -");
+    }
+    public static boolean oyunbittimi(){
+        for(int i =0; i<4 ; i++){
+            for(int j = 0; j<4; j++){
+                if (kartlar[i][j].isTahmin()== false) {
+                    return false;
+                }
+            }
+               
+        }
+        return true;
+    }
+    
+
+}
