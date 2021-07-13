6bbfe6327cf8822a38815630277ecf33bd03f780
alialpay <alialpay96@hotmail.com>
2021-07-14 00:12:36 +0300

    interfaces

---
+++ Interfaces_detail/src/IDeneme.java
@@ -0,0 +1,17 @@
+
+public interface IDeneme {
+    public int a = 4;
+    
+            // private int b =4;  olamaaazzzz
+
+    // public int b; interfacede tanımlıyosan orda değer verecen
+    
+    public static String deneme = "Deneme";  // static yazmaya gerek yok
+    
+    public final int c = 6;  // final yazmaya gerek yok
+    
+    public static void deneme() {
+        System.out.println("Denemdkalhge");
+    }
+    
+}
---
+++ Interfaces_detail/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        
+        
+        System.out.println(IDeneme.a);
+        System.out.println(IDeneme.deneme);
+        System.out.println(IDeneme.c);
+       
+        IDeneme.deneme();
+    }
+}
