772cc858aa98a509942b17fd9a95c7fe97d6dcb6
alialpay <alialpay96@hotmail.com>
2021-07-13 23:53:55 +0300

    final

---
+++ Final_keyword2/src/Database.java
@@ -0,0 +1,8 @@
+
+public  class Database {
+    public void baglantiKur(String username,String password) {
+        
+        System.out.println(username);
+        System.out.println(password);
+    }
+}
---
+++ Final_keyword2/src/Main.java
@@ -0,0 +1,6 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Database database = new Database();
+        database.baglantiKur("root","123");    }
+}
